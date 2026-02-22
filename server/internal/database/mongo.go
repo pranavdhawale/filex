89adf00b59491d452ac3efe42ecf0b8fecc199c6
Pranav Dhawale <pranavdhawale19@gmail.com>
2026-02-22 22:18:12 +0530

    feat(server): migrate to MongoDB Go Driver v2
    Updated connection logic, repository methods, and error handling for v2 compatibility.

--- server/internal/database/mongo.go
+++ server/internal/database/mongo.go
@@ -5,18 +5,16 @@ import (
 	"log/slog"
 	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 // Connect connects to the MongoDB server at the given URI.
 func Connect(ctx context.Context, uri string) (*mongo.Client, error) {
 	slog.Debug("Connecting to MongoDB", "uri", uri)
 
-	clientOptions := options.Client().ApplyURI(uri)
-
-	client, err := mongo.Connect(ctx, clientOptions)
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
 	if err != nil {
 		return nil, err
 	}
--- server/internal/repository/multipart_repo.go
+++ server/internal/repository/multipart_repo.go
@@ -6,9 +6,9 @@ import (
 	"time"
 
 	"github.com/pranavdhawale/bytefile/internal/models"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type MultipartRepository struct {
